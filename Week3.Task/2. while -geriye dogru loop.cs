655c0f443f891cb04e4a0fb14a93ab7d982d9aeb
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-01 21:10:57 +0400

    Add files via upload

---
+++ Week3.Task/2. while -geriye dogru loop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num > 0)
+            {
+                Console.WriteLine(num);
+                num--;
+            }
+
+        }
+    }
+}
