d92f140b7d731729d110e94621b626fbd804cadf
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-01 23:46:24 +0400

    Add files via upload

---
+++ Week3.Task/WhileFactorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Training_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 7;
+            int factorial = 1;
+            while(true)
+            {
+                if (n <= 1)
+                {
+                    break;
+                }
+                factorial *= n;
+                n--;
+            }
+            Console.WriteLine(factorial);
+            
+        }
+    }
+}
