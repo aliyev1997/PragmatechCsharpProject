724b7d1ca069b467c730c300072b368a5782eb52
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-07 01:23:25 +0400

    Add files via upload

---
+++ Week3.Task/task8_vurma_cedveli.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8_vurma_cedveli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x < 11)
+            {
+
+                int i = 0;
+                while (i < 11)
+                {
+                    Console.Write(x + "*" + i + "=" + x * i+" ");
+                    i++;
+                }
+                Console.WriteLine();
+                x++;
+
+            }
+
+        }
+    }
+}
