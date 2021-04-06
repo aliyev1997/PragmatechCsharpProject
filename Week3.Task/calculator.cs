8c578f5da0cf74b07b18ac1ed8b4c5b82fc9b2bd
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-07 01:43:29 +0400

    Add files via upload

---
+++ Week3.Task/calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose one of the these operations: +, -, *, /. Enter operation type to perform: ");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Enter first number: ");
+            int x = int.Parse( Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int y  = int.Parse( Console.ReadLine());
+
+
+
+
+
+            switch (operation)
+            {
+                case ("+"):
+                    Console.WriteLine("Result: "+(x+y));
+                    break;
+                case ("-"):
+                    Console.WriteLine("Result: " + (x - y));
+                    break;
+                case ("*"):
+                    Console.WriteLine("Result: " + (x *y));
+                    break;
+                case ("/"):
+                    Console.WriteLine("Result: " + (x / y));
+                    break;
+                default:
+                    Console.WriteLine("Please choose one of the options: +, -, *, /. ");
+
+                    break;
+            }
+        }
+    }
+}
