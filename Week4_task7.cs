682fb333fea8da0875f16f236a99b07ac7ba9bf4
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-08 13:16:10 +0400

    Add files via upload

---
+++ Week4_task7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter number");
+            int y = int.Parse(Console.ReadLine());
+
+            int[] array = new int[x];
+            int i = 0;
+            while (i < x)
+            {
+                array[i] = y;
+                i++;
+            }
+           
+                Console.WriteLine(array.Sum());
+            
+
+
+        }
+    }
+}
