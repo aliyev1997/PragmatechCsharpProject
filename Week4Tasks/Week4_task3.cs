c9e70843ddba782c1982d1eb3760c36123ab312d
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-08 15:58:21 +0400

    Add files via upload

---
+++ Week4Tasks/Week4_task3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 6, 9, 12, 15, 7, 3, 20, 14, 36, 24 };
+
+            int i = 1;
+            
+
+            while (i < arr.Length+1)
+            {
+                Console.WriteLine(arr[arr.Length - i]);
+                    i++;
+            }
+
+
+        }
+    }
+}
---
+++ Week4Tasks/Week4_task5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int i = 0;
+            int x = 0;
+            int[] numbers = new int [10];
+            while(i<11)
+            {
+                if (i % 2 != 0)
+                {
+                    int m = i * i * i;
+                    
+                    numbers[x] = m;
+                    x++;
+                    i++;
+                                     
+                         }
+                else
+                {
+                    i++;
+                }
+            }
+
+                Console.WriteLine(numbers.Sum());
+            
+
+
+                
+                
+                
+                
+
+
+
+            
+        
+}
+    }
+}
