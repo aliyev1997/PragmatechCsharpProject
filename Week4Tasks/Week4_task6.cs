5b99927d3f6741e2514f7a4383139dc72c7bf529
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-08 16:41:20 +0400

    Add files via upload

---
+++ Week4Tasks/Week4_task6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DateTime v = DateTime.Now;
+            int year = v.Year;
+            Console.WriteLine("Enter your birth year: ");
+            int old = int.Parse(Console.ReadLine());
+            Console.Write(year-old);
+
+        }
+    }
+}
