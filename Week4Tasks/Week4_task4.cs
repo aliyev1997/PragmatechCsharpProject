c1dc96e05218af691f5dc3faf59aaa1078db331b
aliyev1997 <74723219+aliyev1997@users.noreply.github.com>
2021-04-23 18:16:15 +0400

    Update Week4_task4.cs

--- Week4Tasks/Week4_task4.cs
+++ Week4Tasks/Week4_task4.cs
@@ -9,8 +9,7 @@ namespace ConsoleApp8
 
             while (true)
             {
-                int[] money = new int[7] { 200, 100, 50, 20, 10, 5, 1 };
-
+               
                 Console.WriteLine("Istediyiniz meblegi daxil edin: ");
                 string want = Console.ReadLine();
 
@@ -22,6 +21,7 @@ namespace ConsoleApp8
                 else
                 {
                     int user_request = int.Parse(want);
+                    //method Balans(); can be used
                     for (int i = 0; i < money.Length; i++)
                     {
                         if (user_request >= money[i])
@@ -37,3 +37,20 @@ namespace ConsoleApp8
         }
     }
 }
+
+
+//method
+// public static void Balans(int user_request)
+      // {
+      //     int[] money = new int[7] { 200, 100, 50, 20, 10, 5, 1 };
+      //     for (int i = 0; i < money.Length; i++)
+      //     {
+      //         if (user_request >= money[i])
+      //         {
+      //             int bankomat = user_request / money[i];
+      //             user_request -= bankomat * money[i];
+      //             Console.WriteLine(bankomat + " - " + money[i]);
+      //         }
+      //         
+      //     }
+      // }
